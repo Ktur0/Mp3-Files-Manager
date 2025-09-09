6c7837bd1e569c38d50c31c38c5d20994f951189
Ktur0 <ruaconminecraft@gmail.com>
2025-09-09 20:03:10 +0700

    Added click event and fix auto load mp3 files

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import os
 
 # Initialize Pygame
 pygame.init()
-
-screenInfo = (460, 400)
+screenInfo = (550, 400)
 screen = pygame.display.set_mode(screenInfo, pygame.RESIZABLE)
 run = True
 
 # Load sound files
 soundFolder = 'mp3_files'
 soundFilesPath = []
+pygame.mixer.init()
 for i in os.listdir(soundFolder):
     if i.endswith('.mp3'):
-        soundFilesPath.append(pygame.mixer.Sound(os.path.join(soundFolder, i)))
-
+        try:
+            soundFilesPath.append(pygame.mixer.Sound(os.path.join(soundFolder, i)))
+        except Exception as e:
+            print(f"Lỗi khi load {i}: {e}")
 # Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -31,19 +33,36 @@ buttonSizeX, buttonSizeY = 100, 100
 nameFilesFrame = []
 openFilesButton = []
 
+# Mouse
+mouseX, mouseY = 0,0
+mouseSize = 10
+mouseBox = pygame.Rect(mouseX, mouseY, mouseSize, mouseSize)
+
+# Generate frames and buttons based on number of sound files
 for i in range(len(soundFilesPath)):
     nameFilesFrame.append(pygame.Rect(frameX, frameY + i*(frameSizeY + frameY), frameSizeX, frameSizeY))
     openFilesButton.append(pygame.Rect(buttonX, buttonY + i*(buttonSizeY + frameY), buttonSizeX, buttonSizeY))
 
 while run:
 
+    # Update mouse position
+    mouseX, mouseY = pygame.mouse.get_pos()
+    mouseBox = pygame.Rect(mouseX, mouseY, mouseSize, mouseSize)
+
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             run = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range (len(openFilesButton)):
+                if mouseBox.colliderect(openFilesButton[i]):
+                    pygame.mixer.stop()  # Dừng tất cả âm thanh đang phát
+                    soundFilesPath[i].play()  # Phát file mới
 
     screen.fill(white)
 
+    # Draw frames and buttons
     for frame in nameFilesFrame:
         pygame.draw.rect(screen, grey, frame)
     
